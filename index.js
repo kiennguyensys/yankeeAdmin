ae81d620ed4c313ac5772a6c99b0dbf629592c11
ADMIN <admin@QBs-MacBook-Pro.local>
2019-11-17 10:19:49 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const { Keystone } = require('@keystonejs/keystone');
+const { PasswordAuthStrategy } = require('@keystonejs/auth-password');
+const { Text, Checkbox, Password, Relationship, Float, CloudinaryImage } = require('@keystonejs/fields');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { KnexAdapter: Adapter } = require('@keystonejs/adapter-knex');
+
+const PROJECT_NAME = "yankeesim";
+const { User, Product, Post, PostCategory, Comment } = require('./schema');
+
+
+const keystone = new Keystone({
+  name: PROJECT_NAME,
+    adapter: new Adapter(),
+});
+
+keystone.createList('User', User);
+keystone.createList('Product', Product);
+keystone.createList('Post', Post);
+keystone.createList('PostCategory', PostCategory);
+keystone.createList('Comment', Comment);
+
+keystone.createItems({
+    User: [{name: 'kiennguyensys', email: 'tkien2703@gmail.com', isAdmin: true, password: '123456abc'}]
+});
+
+const authStrategy = keystone.createAuthStrategy({
+  type: PasswordAuthStrategy,
+  list: 'User',
+});
+
+module.exports = {
+  keystone,
+  apps: [
+    new GraphQLApp(),
+    // To create an initial user you can temporarily remove the authStrategy below
+    new AdminUIApp({ enableDefaultRoute: true, authStrategy }),
+  ],
+};
---
+++ schema.js
@@ -0,0 +1,123 @@
+const {
+  File,
+  Float,
+  Text,
+  Slug,
+  Relationship,
+  Select,
+  Password,
+  Checkbox,
+  CalendarDay,
+  DateTime,
+  OEmbed,
+} = require('@keystonejs/fields');
+const { Wysiwyg } = require('@keystonejs/fields-wysiwyg-tinymce');
+
+//Access Control
+const userIsAdmin = ({ authentication: { item: user } }) => Boolean(user && user.isAdmin);
+const userOwnsItem = ({ authentication: { item: user } }) => {
+  if (!user) {
+    return false;
+  }
+  return { id: user.id };
+};
+const userIsAdminOrOwner = auth => {
+  const isAdmin = access.userIsAdmin(auth);
+  const isOwner = access.userOwnsItem(auth);
+  return isAdmin ? isAdmin : isOwner;
+};
+const access = { userIsAdmin, userOwnsItem, userIsAdminOrOwner };
+
+
+exports.User = {
+  fields: {
+    name: { type: Text },
+    email: {
+      type: Text,
+      isUnique: true,
+    },
+    isAdmin: { type: Checkbox },
+    password: {
+      type: Password,
+    },
+  },
+  //To create an initial user you can temporarily remove access controls
+  // access: {
+  //   read: access.userIsAdminOrOwner,
+  //   update: access.userIsAdminOrOwner,
+  //   create: access.userIsAdmin,
+  //   delete: access.userIsAdmin,
+  //   auth: true,
+  // },
+}
+
+exports.Product = {
+  fields: {
+    title: { type: Text },
+    price: { type: Float},
+    image: { type: Text },
+    imageHover: { type: Text },
+    description: { type: Text },
+    updated: { type: DateTime, format: 'DD/MM/YYYY' },
+  },
+  adminConfig: {
+    defaultPageSize: 20,
+    defaultColumns: 'title, price',
+    defaultSort: 'title',
+  },
+  labelResolver: item => item.title,
+};
+
+exports.Post = {
+  fields: {
+    title: { type: Text },
+    image:{ type: Text },
+    slug: { type: Slug, from: 'title' },
+    brief_description: { type: Text },
+    author: {
+      type: Relationship,
+      ref: 'User',
+    },
+    categories: {
+      type: Relationship,
+      ref: 'PostCategory',
+      many: true,
+    },
+    status: {
+      type: Select,
+      defaultValue: 'draft',
+      options: [{ label: 'Draft', value: 'draft' }, { label: 'Published', value: 'published' }],
+    },
+    body: { type: Wysiwyg },
+    posted: { type: DateTime, format: 'DD/MM/YYYY' },
+  },
+  adminConfig: {
+    defaultPageSize: 20,
+    defaultColumns: 'title, status',
+    defaultSort: 'title',
+  },
+  labelResolver: item => item.title,
+};
+
+exports.PostCategory = {
+  fields: {
+    name: { type: Text },
+    slug: { type: Slug, from: 'name' },
+  },
+};
+
+exports.Comment = {
+  fields: {
+    body: { type: Text, isMultiline: true },
+    originalPost: {
+      type: Relationship,
+      ref: 'Post',
+    },
+    author: {
+      type: Relationship,
+      ref: 'User',
+    },
+    posted: { type: DateTime },
+  },
+  labelResolver: item => item.body,
+};
