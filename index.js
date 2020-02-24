5a1f8127a62545685a1214318e8f0507890a5ebb
ADMIN <admin@QBs-MacBook-Pro.local>
2020-02-24 19:41:24 +0700

    switch mongod

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const { User, Product, ProductCategory, ProductTag, Order, Post, PostCategory, P
 const keystone = new Keystone({
   name: 'yankeesim',
   adapter: new MongooseAdapter(),
+  cookieSecret: secret
 });
 
 keystone.createList('User', User);
@@ -40,7 +41,7 @@ keystone.createList('ContactForm', ContactForm);
 
 
 // keystone.createItems({
-//     User: [{name: 'kiennguyensys', email: 'tkien2703@gmail.com', isAdmin: true, password: '123456abc'}]
+//     User: [{name: 'admin', email: 'admin@yankeesim.com', isAdmin: true, password: 'admin123'}]
 // });
 
 const authStrategy = keystone.createAuthStrategy({
@@ -54,7 +55,7 @@ const authStrategy = keystone.createAuthStrategy({
 
 const graphQL = new GraphQLApp()
 
-const admin = new AdminUIApp();
+const admin = new AdminUIApp({ authStrategy, enableDefaultRoute: true, isAccessAllowed: ({ authentication: { item: user, listKey: list } }) => !!user && !!user.isAdmin });
 
 const staticApp = new StaticApp({
       path: "/uploader",
@@ -104,86 +105,86 @@ const preparations = [graphQL, admin, staticApp].map(app =>
 
 Promise.all(preparations).then(async middlewares => {
     await keystone.connect();
-    const server = express();
+    // const server = express();
 
-    server.use(session({
-      secret: 'my super secret',
-      resave: false,
-      saveUninitialized: true,
-    }))
+    // server.use(session({
+    //   secret: 'my super secret',
+    //   resave: false,
+    //   saveUninitialized: true,
+    // }))
 
 
-    server.use(middlewares).listen(port);   
+    // server.use(middlewares).listen(port);   
 
-    server.use(bodyParser.json())
-    server.use(bodyParser.urlencoded({ extended: false }));
+    // server.use(bodyParser.json())
+    // server.use(bodyParser.urlencoded({ extended: false }));
 
-    var storage = multer.diskStorage({ //multers disk storage settings
-        destination: function (req, file, cb) {
-            cb(null, './uploads/')
-        },
-        filename: function (req, file, cb) {
-            var datetimestamp = Date.now();
-            cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1])
-        }
-    });
+    // var storage = multer.diskStorage({ //multers disk storage settings
+    //     destination: function (req, file, cb) {
+    //         cb(null, './uploads/')
+    //     },
+    //     filename: function (req, file, cb) {
+    //         var datetimestamp = Date.now();
+    //         cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1])
+    //     }
+    // });
 
-    var upload = multer({ //multer settings
-                    storage: storage,
-                    fileFilter : function(req, file, callback) { //file filter
-                        if (['xls', 'xlsx'].indexOf(file.originalname.split('.')[file.originalname.split('.').length-1]) === -1) {
-                            return callback(new Error('Wrong extension type'));
-                        }
-                        callback(null, true);
-                    }
-                }).single('file');
+    // var upload = multer({ //multer settings
+    //                 storage: storage,
+    //                 fileFilter : function(req, file, callback) { //file filter
+    //                     if (['xls', 'xlsx'].indexOf(file.originalname.split('.')[file.originalname.split('.').length-1]) === -1) {
+    //                         return callback(new Error('Wrong extension type'));
+    //                     }
+    //                     callback(null, true);
+    //                 }
+    //             }).single('file');
 
-    /** API path that will upload the files */
-    server.post('/upload', function(req, res) {
+    // /** API path that will upload the files */
+    // server.post('/upload', function(req, res) {
 
-        var exceltojson;
-        upload(req,res,function(err){
-            if(err){
-                 res.json({error_code:1,err_desc:err});
-                 return;
-            }
+    //     var exceltojson;
+    //     upload(req,res,function(err){
+    //         if(err){
+    //              res.json({error_code:1,err_desc:err});
+    //              return;
+    //         }
 
-            const listName = req.body.listname
+    //         const listName = req.body.listname
 
-            /** Multer gives us file info in req.file object */
-            if(!req.file){
-                res.json({error_code:1,err_desc:"No file passed"});
-                return;
-            }
-            /** Check the extension of the incoming file and 
-             *  use the appropriate module
-             */
-            if(req.file.originalname.split('.')[req.file.originalname.split('.').length-1] === 'xlsx'){
-                exceltojson = xlsxtojson;
-            } else {
-                exceltojson = xlstojson;
-            }
-            console.log(req.file.path);
-            try {
-                exceltojson({
-                    input: req.file.path,
-                    output: null, //since we don't need output.json
-                }, function(err,result){
-                    if(err) {
-                        return res.json({error_code:1,err_desc:err, data: null});
-                    } 
-                    res.json({error_code:0,err_desc:null, data: result});
+    //         /** Multer gives us file info in req.file object */
+    //         if(!req.file){
+    //             res.json({error_code:1,err_desc:"No file passed"});
+    //             return;
+    //         }
+    //         /** Check the extension of the incoming file and 
+    //          *  use the appropriate module
+    //          */
+    //         if(req.file.originalname.split('.')[req.file.originalname.split('.').length-1] === 'xlsx'){
+    //             exceltojson = xlsxtojson;
+    //         } else {
+    //             exceltojson = xlstojson;
+    //         }
+    //         console.log(req.file.path);
+    //         try {
+    //             exceltojson({
+    //                 input: req.file.path,
+    //                 output: null, //since we don't need output.json
+    //             }, function(err,result){
+    //                 if(err) {
+    //                     return res.json({error_code:1,err_desc:err, data: null});
+    //                 } 
+    //                 res.json({error_code:0,err_desc:null, data: result});
 
-                    // start uploading to graphql server
-                    DBUpload(listName, result)
+    //                 // start uploading to graphql server
+    //                 DBUpload(listName, result)
 
-                 });
-            } catch (e){
-                res.json({error_code:1,err_desc:"Corupted excel file"});
-            }
-        })
+    //              });
+    //         } catch (e){
+    //             res.json({error_code:1,err_desc:"Corupted excel file"});
+    //         }
+    //     })
        
-    });
+    // });
 
 
 });
